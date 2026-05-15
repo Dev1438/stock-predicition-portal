998bcbe528a58b2c346c91c94cc9088674dfdea7
Dev1438 <devender.bakathatla@gmail.com>
2026-05-15 14:44:14 +0530

    homepage

---
+++ frontend-react/src/assets/components/Button.jsx
@@ -0,0 +1,8 @@
+
+const Button = (props) => {
+  return (
+    <a className={`btn ${props.class}`}>{props.text}</a>
+  )
+}
+
+export default Button
---
+++ frontend-react/src/assets/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer py-3 my-3" >
+        <hr className="border-bottom" />
+        <p className="text-light text-center">&copy; 2026 - Bulit by Devender</p>
+
+      </footer>
+      
+    </>
+  )
+}
+
+export default Footer
